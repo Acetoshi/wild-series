de9a28df3bb6800cd181dc1c2d40220f21909944
AkiraAcetonoshi <facilitateurmaker@gmail.com>
2024-05-30 12:49:47 +0200

    modified schema.sql to create program and category tables

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,7 +1,9 @@
 create table user (
   id int unsigned primary key auto_increment not null,
+  username varchar(255) not null unique,
   email varchar(255) not null unique,
-  password varchar(255) not null
+  password varchar(255) not null,
+  bio text 
 );
 
 create table item (
@@ -9,4 +11,20 @@ create table item (
   title varchar(255) not null,
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
+);
+
+create table category (
+  id int unsigned primary key auto_increment not null,
+  name varchar(255) not null
+);
+
+create table program (
+  id int unsigned primary key auto_increment not null,
+  title varchar(255) not null,
+  synopsis TEXT not null,
+  poster varchar(255) not null,
+  country varchar(255) not null,
+  program_year varchar(255) not null,
+  category_id int unsigned not null,
+  foreign key(category_id) references category(id)
 );
